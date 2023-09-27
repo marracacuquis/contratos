66c4b01eb24b7a7d8c210aaa7a030f0cc592201d
marracacuquis <146114663+marracacuquis@users.noreply.github.com>
2023-09-27 12:01:12 +0200

    Add files via upload

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-contract",
+    description="Meta package for oca-contract Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-contract>=16.0dev,<16.1dev',
+        'odoo-addon-contract_invoice_start_end_dates>=16.0dev,<16.1dev',
+        'odoo-addon-contract_sale>=16.0dev,<16.1dev',
+        'odoo-addon-contract_sale_generation>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
